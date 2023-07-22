added decorators to capture logs, fixed data type issues with Portfolio calculation

server/src/finance/data_fetcher.py
@@ -1,6 +1,5 @@
 from typing import Tuple, List, Callable
 from datetime import datetime
-import numpy as np
 import pandas as pd
 import yfinance as yf
 import functools
@@ -12,6 +11,7 @@ if __name__ == "__main__":
     from utils import is_list_of_type, is_tuple_of_type
 else:
     from .utils import is_list_of_type, is_tuple_of_type
+
 
 def data_cache(maxsize: int = 128, expiration_seconds: int | float = 3600) -> Callable:
     """A custom cache decorator that caches the results of function evaluations for a specified duration and with a maximum cache size.
@@ -36,9 +36,10 @@ def data_cache(maxsize: int = 128, expiration_seconds: int | float = 3600) -> Ca
     def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            normalized_args = tuple(*args) if np.all([isinstance(arg, list) for arg in args]) else args
+            normalized_args = tuple(*args) if is_tuple_of_type(args, list) or is_tuple_of_type(args, tuple) else args
             normalized_kwargs = frozenset(kwargs.items())
             key = (normalized_args, normalized_kwargs)
+
             # Check if the result is already cached
             with lock:
                 if key in cache:
@@ -69,7 +70,7 @@ def data_cache(maxsize: int = 128, expiration_seconds: int | float = 3600) -> Ca
 
 
 @data_cache(maxsize=512, expiration_seconds=7200)
-def get_tickers(tickers: Tuple[str] | List[str] | str, start: datetime | str = "2023-01-01", period: str = "1d") -> pd.DataFrame:
+def get_tickers(tickers: Tuple[str] | List[str] | str, start: datetime | str = "2018-01-01", period: str = "1d") -> pd.DataFrame:
     """Retrieve the tickers data
 
     Args:

server/src/finance/financial_models.py
@@ -1,11 +1,9 @@
-import sys
 import pandas as pd
 import numpy as np
 from scipy.stats import norm
 from .utils import is_psd, is_matching_index
-
-sys.path.insert(0, "..")
 from exceptions import MismatchedIndexException
+
 
 def validate_weights(w: pd.Series) -> None:
     """Validate if the provided weights w is valid subject to practical constraints.

server/src/finance/utils.py
@@ -75,12 +75,16 @@ def normalize(s: pd.Series | np.ndarray) -> pd.Series:
     return s / s.sum()
 
 
+def is_iterable_of_type(var: Any, itype: Type, dtype: Type):
+    return isinstance(var, itype) and np.all([isinstance(item, dtype) for item in var])
+
+
 def is_list_of_type(var: Any, dtype: Type) -> bool:
-    return isinstance(var, list) and np.all([isinstance(item, dtype) for item in var])
+    return is_iterable_of_type(var, list, dtype)
 
 
 def is_tuple_of_type(var: Any, dtype: Type) -> bool:
-    return isinstance(var, tuple) and np.all([isinstance(item, dtype) for item in var])
+    return is_iterable_of_type(var, tuple, dtype)
 
 
 def is_close(d1: pd.DataFrame | pd.Series | float, d2: pd.DataFrame | pd.Series | float, threshold: float = 1e-5) -> bool:

server/tests/test_utils.py
@@ -17,7 +17,7 @@ def s_series(s_ndarray):
 class TestIsPSD:
     @pytest.fixture
     def X_ndarray(self):
-        A = np.random.rand(10, 10)
+        A = np.random.rand(10, 1)
         X = A @ A.T
         return X
 
@@ -60,3 +60,11 @@ class TestNormalize:
     def test_normalize_negative_ndarray(self, arr, expected):
         with pytest.raises(ValueError):
             _ = utils.normalize(-arr)
+
+
+class TestIsListOfType:
+    pass
+
+
+class TestIsTupleOfType:
+    pass